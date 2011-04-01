bdca5e265420ae899f548850f993f718949c3a09
esbenbugge@gmail.com <esbenbugge@gmail.com@b9cbcbe6-fdd0-c497-30a1-be04bb53b980>
2011-04-01 15:10:31 +0000

    A lot of different work after finding performance issue in fextract_helper: The entire array of freqs. of all n-grams was being traversed on feature extraction from every text. Moved corpus_analysis to fextract

--- python/fextract_pupy.py
+++ python/fextract_pupy.py
@@ -1,10 +1,12 @@
 from mpi import MPI
 from nltk.corpus import PlaintextCorpusReader
+from nltk.probability import FreqDist
 import fextract_helper
 import subprocess
 import os
 import sys
 import time
+
 
 wrd_ngrams = False
 wrd_ngram_size = 3
@@ -49,7 +51,7 @@ def splitup_tasks(all_tasks, workers, tasks_per_worker):
 # Init MPI system
 mpi = MPI()
 workers = mpi.MPI_COMM_WORLD
-nworkers = workers.size()
+nworkers = workers.size()-1
 rank = workers.rank() # The rank of the current process
 
 # Read arguments from command line
@@ -120,80 +122,288 @@ if rank == 0:
     
 if rank == 0:
     text_classes = fextract_helper.find_classes(texts)
-    
-
-# Features: char n-grams
+   
+# Using master
 if char_ngrams:
-    # Split up texts and process find char n-grams in subset of texts in each process
-    all_char_ngrams = []
-    text_char_ngrams = []
-    a, t = fextract_helper.char_ngram_stats(text_tasks[rank], corpus, n_char_ngrams, char_ngram_size)
-    r1 = workers.allgather(a)
-    for r in r1:
-        all_char_ngrams.extend(r)
-    r2 = workers.allgather(t)
-    for r in r2:
-        text_char_ngrams.extend(r)
-    
-    # Each process creates features for a subset of texts
-    cng_tasks = splitup_tasks(text_char_ngrams, nworkers, tasks_per_worker)
-    f = fextract_helper.create_char_ngrams(n_char_ngrams, all_char_ngrams, cng_tasks[rank])
-    r3 = workers.allgather(f)
+        
     if rank == 0:
+        s1 = time.time()
+        #print rank, 'Finding n-grams'
+        all_ngs = fextract_helper.get_cngs(texts,corpus_root,char_ngram_size)
+        e1 = time.time()
+        print rank, 'Finding took', e1-s1, 'seconds'
+        #print rank, 'Calculating most frequent n-grams'
+        print len(all_ngs)
+        s1 = time.time()
+        all_ngs_freqs = FreqDist(all_ngs)
+        tot_cngs = min([n_char_ngrams, all_ngs_freqs.B()])
+        mostfreq_ngs = all_ngs_freqs.keys()[:tot_cngs]
+        e1 = time.time()
+        print rank, 'Calc took', e1-s1, 'seconds'
+        #print rank, 'Sending most-frequent ngrams'
+        s1 = time.time()
+        workers.bcast(mostfreq_ngs,0)
+        e1 = time.time()
+        #print rank, 'Bcast took', e1-s1, 'seconds'
+        f = []
+    else:
+        #print rank, 'Finding n-grams'
+        s1 = time.time()
+        t = fextract_helper.get_text_cngs(text_tasks[rank-1],corpus_root,char_ngram_size)
+        e1 = time.time()
+        print rank, 'Finding took', e1-s1, 'seconds'
+        #print rank, 'Receiving most-frequent ngrams'
+        s1 = time.time()
+        mostfreq_ngs = workers.bcast(root=0)
+        e1 = time.time()
+        #print rank, 'Bcast took', e1-s1, 'seconds'
+        #print rank, 'Creating features'
+        s1 = time.time()
+        f = fextract_helper.create_ngram_feats(mostfreq_ngs, t)
+        e1 = time.time()
+        print rank, 'Creating took', e1-s1, 'seconds' 
+    
+    #print rank, 'Gathering features'
+    s1 = time.time()
+    r2 = workers.gather(f,0)
+    e1 = time.time()
+    #print rank, 'Gather took', e1-s1, 'seconds'
+    if rank == 0:    
         charngram_features = []
-        for r in r3:
+        for r in r2:
             charngram_features.extend(r)
-        
-# Features: word n-grams
+            
 if wrd_ngrams:
-    # Split up texts and process find word n-grams in subset of texts in each process
-    all_wrd_ngrams = []
-    text_wrd_ngrams = []
+        
+    print rank, 'Finding word n-grams'
+    all_ngs = []
     a, t = fextract_helper.wrd_ngram_stats(text_tasks[rank], corpus, n_wrd_ngrams, wrd_ngram_size)
+    print rank, 'Sending n-grams'
     r1 = workers.allgather(a)
     for r in r1:
-        all_wrd_ngrams.extend(r)
-    r2 = workers.allgather(t)
-    for r in r2:
-        text_wrd_ngrams.extend(r)
-    
-    # Each process creates features for a subset of texts
-    wng_tasks = splitup_tasks(text_wrd_ngrams, nworkers, tasks_per_worker)
-    f = fextract_helper.create_wrd_ngrams(n_wrd_ngrams, all_wrd_ngrams, wng_tasks[rank])
-    r3 = workers.allgather(f)
-    if rank == 0:
+        all_ngs.extend(r)
+    print rank, 'Calculating most frequent word n-grams'
+    all_ngs_freqs = FreqDist(all_ngs)
+    tot_wngs = min([n_wrd_ngrams, all_ngs_freqs.B()])
+    mostfreq_ngs = all_ngs_freqs.keys()[:tot_wngs]
+    print rank, 'Creating features for word n-grams'
+    f = fextract_helper.create_ngram_feats(mostfreq_ngs, t)
+    print rank, 'Sending features'  
+    r2 = workers.gather(f,0)
+    if rank == 0:    
         wrdngram_features = []
-        for r in r3:
-            wrdngram_features.extend(r)
-        
-# TODO: Features: function words
-if function_words:
-    text_fws = []
-    f, t = fextract_helper.extract_fws(text_tasks[rank], corpus, cur_dir + "/" + FUNC_FOLDER)
-    r1 = worker.allgather(t)
-    for r in r1:
-        text_fws.extend(r)
-    # Create features
-    fw_tasks = splitup_tasks(text_fws, nworkers, tasks_per_worker)
-    fm = fextract_helper.create_fw_features(f, fw_tasks[rank])
-    r2 = workers.allgather(fm)
-    if rank == 0:
-        fw_features = []
         for r in r2:
-            fw_features.extend(r)
+            wrdngram_features.extend(r) 
+
+## Using allgather
+#if char_ngrams:
+#        
+#    print rank, 'Finding n-grams'
+#    all_ngs = []
+#    a, t = fextract_helper.char_ngram_stats(text_tasks[rank], corpus, n_char_ngrams, char_ngram_size)
+#    print rank, 'Sending n-grams'
+#    r1 = workers.allgather(a)
+#    for r in r1:
+#        all_ngs.extend(r)
+#    print rank, 'Calculating most frequent n-grams'
+#    all_ngs_freqs = FreqDist(all_ngs)
+#    tot_cngs = min([n_char_ngrams, all_ngs_freqs.B()])
+#    mostfreq_ngs = all_ngs_freqs.keys()[:tot_cngs]
+#    print rank, 'Creating features'
+#    f = fextract_helper.create_ngram_feats(mostfreq_ngs, t)  
+#    print rank, 'Sending features'
+#    r2 = workers.gather(f,0)
+#    if rank == 0:    
+#        charngram_features = []
+#        for r in r2:
+#            charngram_features.extend(r)
+#            
+#if wrd_ngrams:
+#        
+#    print rank, 'Finding word n-grams'
+#    all_ngs = []
+#    a, t = fextract_helper.wrd_ngram_stats(text_tasks[rank], corpus, n_wrd_ngrams, wrd_ngram_size)
+#    print rank, 'Sending n-grams'
+#    r1 = workers.allgather(a)
+#    for r in r1:
+#        all_ngs.extend(r)
+#    print rank, 'Calculating most frequent word n-grams'
+#    all_ngs_freqs = FreqDist(all_ngs)
+#    tot_wngs = min([n_wrd_ngrams, all_ngs_freqs.B()])
+#    mostfreq_ngs = all_ngs_freqs.keys()[:tot_wngs]
+#    print rank, 'Creating features for word n-grams'
+#    f = fextract_helper.create_ngram_feats(mostfreq_ngs, t)
+#    print rank, 'Sending features'  
+#    r2 = workers.gather(f,0)
+#    if rank == 0:    
+#        wrdngram_features = []
+#        for r in r2:
+#            wrdngram_features.extend(r)
         
-# Process with rank 0 wraps it all up by creating the feature matrix
+    
+#    # Find n-grams in provided texts
+#    #a, t = fextract_helper.char_ngram_stats(text_tasks[rank],corpus,n_char_ngrams,char_ngram_size)
+#    # Master finds most occurring n-grams across all texts
+#    if rank == 0:
+#        print 'Master finding all char n-grams'
+#        ngs = fextract_helper.get_cngs(texts,corpus,char_ngram_size)
+#        freqs = FreqDist(ngs)
+#        tot_ngrams = min([n_char_ngrams, freqs.B()])
+#        mostfreq_ngs = freqs.keys()[:tot_ngrams]
+#        print 'Master found', len(mostfreq_ngs), 'most-occurring char n-grams'
+#        workers.bcast(mostfreq_ngs,0)
+#        cng_feat = [] # Master does not create features
+#    # Other processes find n-grams per text
+#    else: 
+#        text_ngs = fextract_helper.get_text_cngs(text_tasks[rank-1],corpus,char_ngram_size)
+#        mostfreq_ngs = workers.bcast(root=0)
+#        cng_feat = fextract_helper.create_ngram_feats(mostfreq_ngs,text_ngs)
+#        
+#    # Send features to master
+#    features = workers.gather(cng_feat,0)
+#    
+#    # Master creates final feature matrix
+#    if rank == 0:
+#        charngram_features = []
+#        for f in features:
+#            charngram_features.extend(f)
+    
+        
+    # Send found n-grams to master
+    #ngrams = workers.gather(a,0)
+    
+    # Master collects all n-grams in one structure
+    # and broadcasts this structure to all processes
+    #if rank == 0:
+    #    all_ngrams = []
+    #    for g in ngrams:
+    #        all_ngrams.extend(g)
+    #    workers.bcast(all_ngrams,0)
+    #else:
+        #workers.bcast(root=0)
+    
+    #print rank, ': Got', len(all_ngrams), 'ngrams in total'
+    
+    # All processes create features for their texts
+    #my_features = fextract_helper.create_char_ngrams(n_char_ngrams,all_ngrams, t)
+    #my_features = [range(n_char_ngrams) for i in range(len(t))]
+    #print rank, ': My features:', my_features
+    
+    # Send features to master
+    #features = workers.gather(my_features,0)
+    
+    # Master creates final feature matrix
+    #if rank == 0:
+    #    charngram_features = []
+    #    for f in features:
+    #        charngram_features.extend(f)
+    
+    
+    
+#    # Split up texts and process find char n-grams in subset of texts in each process
+#    #all_char_ngrams = []
+##    #text_char_ngrams = []
+#    if rank != 0:
+#        a, t = fextract_helper.char_ngram_stats(text_tasks[rank], corpus, n_char_ngrams, char_ngram_size)
+#        workers.(a, 0)
+#        all_char_ngrams = workers.recv(0, ALL_NGRAMS_TAG)
+#        f = fextract_helper.create_char_ngrams(n_char_ngrams, all_char_ngrams, t)
+#        workers.igather(f,0)
+#    if rank == 0:
+#        
+#        all_char_ngrams = []
+#        for r in r1:
+#            all_char_ngrams.extend(r)
+#        workers.
+#        fhandle = workers.igather(f,0)
+        
+#    s1 = time.time()
+#    r1 = workers.allgather(a)
+#    e1 = time.time()
+#    print os.getpid(), ': Waited for', e1-s1, 'seconds (1)'
+#    for r in r1:
+#        all_char_ngrams.extend(r)
+#    #s2 = time.time()
+#    #r2 = workers.allgather(t)
+#    #e2 = time.time()
+#    #print os.getpid(), ': Waited for', e2-s2, 'seconds (2)'
+#    #for r in r2:
+#    #    text_char_ngrams.extend(r)
+#    
+#    # Each process creates features for a subset of texts
+#    #cng_tasks = splitup_tasks(text_char_ngrams, nworkers, tasks_per_worker)
+#    #f = fextract_helper.create_char_ngrams(n_char_ngrams, all_char_ngrams, cng_tasks[rank])
+#    f = fextract_helper.create_char_ngrams(n_char_ngrams, all_char_ngrams, text_char_ngrams)
+##    s3 = time.time()
+##    r3 = workers.allgather(f)
+##    e3 = time.time()
+##    print os.getpid(), ': Waited for', e3-s3, 'seconds (3)'
+#    if rank == 0:
+#        charngram_features = []
+#        for r in r3:
+#            charngram_features.extend(r)
+#        
+## Features: word n-grams
+#if wrd_ngrams:
+#    # Split up texts and process find word n-grams in subset of texts in each process
+#    all_wrd_ngrams = []
+#    #text_wrd_ngrams = []
+#    a, text_wrd_ngrams = fextract_helper.wrd_ngram_stats(text_tasks[rank], corpus, n_wrd_ngrams, wrd_ngram_size)
+#    s1 = time.time()
+#    r1 = workers.allgather(a)
+#    e1 = time.time()
+#    print os.getpid(), ': Waited for', e1-s1, 'seconds (4)'
+#    for r in r1:
+#        all_wrd_ngrams.extend(r)
+#    #s2 = time.time()
+#    #r2 = workers.allgather(t)
+#    #e2 = time.time()
+#    #print os.getpid(), ': Waited for', e2-s2, 'seconds (5)'
+#    #for r in r2:
+#    #    text_wrd_ngrams.extend(r)
+#    
+#    # Each process creates features for a subset of texts
+#    #wng_tasks = splitup_tasks(text_wrd_ngrams, nworkers, tasks_per_worker)
+#    #f = fextract_helper.create_wrd_ngrams(n_wrd_ngrams, all_wrd_ngrams, wng_tasks[rank])
+#    f = fextract_helper.create_wrd_ngrams(n_wrd_ngrams, all_wrd_ngrams, text_wrd_ngrams)
+#    s3 = time.time()
+#    r3 = workers.allgather(f)
+#    e3 = time.time()
+#    print os.getpid(), ': Waited for', e3-s3, 'seconds (6)'
+#    if rank == 0:
+#        wrdngram_features = []
+#        for r in r3:
+#            wrdngram_features.extend(r)
+#        
+## TODO: Features: function words
+#if function_words:
+#    text_fws = []
+#    f, t = fextract_helper.extract_fws(text_tasks[rank], corpus, cur_dir + "/" + FUNC_FOLDER)
+#    r1 = worker.allgather(t)
+#    for r in r1:
+#        text_fws.extend(r)
+#    # Create features
+#    fw_tasks = splitup_tasks(text_fws, nworkers, tasks_per_worker)
+#    fm = fextract_helper.create_fw_features(f, fw_tasks[rank])
+#    r2 = workers.allgather(fm)
+#    if rank == 0:
+#        fw_features = []
+#        for r in r2:
+#            fw_features.extend(r)
+        
+# 0 wraps it all up by creating the feature matrix
 # and saving output to files
 if rank == 0:
     # Create feature matrix with all features
     feature_matrix = [[] for i in range(n_texts)]
     for f in range(len(feature_matrix)):
+#        pass
         if char_ngrams:
             feature_matrix[f].extend(charngram_features[f])
         if wrd_ngrams:
             feature_matrix[f].extend(wrdngram_features[f])
-        if function_words:
-            feature_matrix[f].extend(fw_features[f])
+#        if function_words:
+#            feature_matrix[f].extend(fw_features[f])
     print 'Features:', len(feature_matrix), "x", len(feature_matrix[0])
             
     # Save features to files
@@ -201,7 +411,7 @@ if rank == 0:
                                   os.path.join(cur_dir,CATEGORY_FILE), text_classes)
     
 
-# Rank 0 stops timer and displays results
+# 0 stops timer and displays results
 if rank == 0:
     endtime = time.time()
     n_features = len(feature_matrix[0])

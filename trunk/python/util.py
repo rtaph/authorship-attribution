e09debb7707840513258430eb309a97304531125
esbenbugge@gmail.com <esbenbugge@gmail.com@b9cbcbe6-fdd0-c497-30a1-be04bb53b980>
2011-04-08 15:24:57 +0000

    Cross-validation fixed in util.py

--- trunk/python/util.py
+++ trunk/python/util.py
@@ -1,4 +1,5 @@
 import math
+import operator
 
 def slr(x, y):
     '''
@@ -16,24 +17,78 @@ def slr(x, y):
     return a, b
 
 
-#TODO: Returns something weird!
-
 def k_fold_cv_ind(classes, K):
     '''
     K-fold CV indices
     '''
     
-    distinct = list(set(classes))
-    next = {}
-    for c in distinct:
-        next[c] = 0
-        
-    ind = []
+    bla = []
     for i in range(len(classes)):
-        c = classes[i]
-        ind.append(next[c])
-        next[c] = (next[c] + 1) % K  
-    return ind 
+        bla.append((classes[i],i))
+    
+    sorted_bla = sorted(bla,key=operator.itemgetter(0))
+    #print sorted_bla
+    
+    ind = [0 for i in range(len(classes))]
+    x = 0
+    for s in sorted_bla:
+        k = s[1]
+        ind[k] = x
+        x = (x + 1) % K
+        
+    #print ' ---', classes
+    #print ' ---', ind
+    return ind
+#    
+#    psize_min = len(classes) / K
+#    psize_max = psize_min + (len(classes) % K)
+#    print 'psize', psize_min, psize_max
+#    prev_count = 0
+    
+#    bins = [{'indices': [], 'classcounts': []} for i in range(K)]
+#    next_bin = 0
+#    
+#    psize_max = psize_min + (len(classes) % K)
+#    i = 0
+#    c = 0
+#    while c < len(classes):
+#        cl = classes[i]
+#        if bins[next_bin]
+
+
+#    sortedcl = sorted(classes)
+#    print sortedcl
+#    
+#    distinct = list(set(classes))
+#    cind = {}
+#    for i in range(len(classes)):
+#        c = classes[i]
+#        if not cind.has_key(c):
+#            cind[c] = 0
+#        cind[c] = cind[c] + 1 
+#    
+#    for c in cind:
+#        len(classes) / K
+    
+    
+    
+#    next = {}
+#    for c in distinct:
+#        next[c] = 0
+#        
+#    ind = []
+##    c_ind = {}
+#    for i in range(len(classes)):
+#        c = classes[i]
+##        if not c_ind.has_key(c):
+##            c_ind[c] = []
+##        c_ind[c].append(i)
+#        ind.append(next[c])
+#        next[c] = (next[c] + 1) % K
+#    #print c_ind
+#    #for c in c_ind:
+#          
+#    return ind 
     
     #for k in xrange(K):
     #    training = [x for i, x in enumerate(X) if i % K != k]
@@ -41,9 +96,23 @@ def k_fold_cv_ind(classes, K):
     #    yield training, validation
     
     
-    
-    
-    
+  
+# TEST
+#k = 4
+#c = [1,1,1,2,2,2,1,1,1,1,4,2,2,1,1,2,3,2,2,2,2,4,3,3,3,3,3,1,1]
+#ind = k_fold_cv_ind(c, k)
+#
+#for i in range(k):
+#    s = 0
+#    t = []
+#    for u in range(len(ind)):
+#        j = ind[u]
+#        if i == j:
+#            s = s + 1
+#            t.append(c[u])
+#    print t
+#    print s
+#    
     
     
     

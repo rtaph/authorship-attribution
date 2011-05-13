6de37b851f5b365eb78d7b2e62aa28b5cb5ab2f8
esbenbugge@gmail.com <esbenbugge@gmail.com@b9cbcbe6-fdd0-c497-30a1-be04bb53b980>
2011-05-13 09:45:30 +0000

    boost.py: Optimization, nb_boost: Now picking training text more randomly

--- python/boost.py
+++ python/boost.py
@@ -17,10 +17,22 @@ class Samme:
         self.K = K
         #print len(features)
         weights = [1/float(no_of_texts) for n in range(no_of_texts)]
-        no_of_classifiers = len(classifiers)
+        self.classifiers = classifiers
+        no_of_classifiers = len(self.classifiers)
         self.M = M
         self.alphas = range(self.M)
         self.selected_classifiers = [None for m in range(self.M)]
+        
+        # Do classification for each of the classifiers
+        classified = []
+        for c in range(no_of_classifiers):
+            myclassified = self.classifiers[c].classify(features[c])
+                
+            # NB classifier returns, for each text, a list of the X most probable
+            # classes. The first element of this list is the most probable class
+            # and the first element of this element is the class name
+            classified.append([x[0][0] for x in myclassified])
+        
         for m in range(self.M):
             
             print 'Weights', weights[:10]
@@ -29,21 +41,12 @@ class Samme:
             
             # Try classifying with all classifiers
             for c in range(no_of_classifiers):
-                #print classes
-                
-                classified = classifiers[c].classify(features[c])
-                
-                # NB classifier returns, for each text, a list of the X most probable
-                # classes. The first element of this list is the most probable class
-                # and the first element of this element is the class name
-                classified = [x[0][0] for x in classified]
-                #print classified
                 
                 # Register the error for this classifier
                 a_sum = 0
                 err_sum = 0
                 for t in range(no_of_texts):
-                    if classified[t] != classes[t]:
+                    if classified[c][t] != classes[t]:
                         err_sum = err_sum + weights[t]
                     else:
                         a_sum = a_sum + 1
@@ -56,10 +59,10 @@ class Samme:
             min_error = min(all_errors)
             
             # Select classifier with lowest error
-            for e in range(no_of_classifiers):
-                if min_error == all_errors[e]:
-                    print 'Selected classifier', e
-                    self.selected_classifiers[m] = classifiers[e]
+            for c in range(no_of_classifiers):
+                if min_error == all_errors[c]:
+                    print 'Selected classifier', c
+                    self.selected_classifiers[m] = c
                     break
             
             # Alpha
@@ -67,7 +70,7 @@ class Samme:
             
             # Weight per text (normalized)
             for i in range(no_of_texts):
-                if classified[i] != classes[i]:
+                if classified[self.selected_classifiers[m]][i] != classes[i]:
                     weights[i] = weights[i] * math.exp(self.alphas[m]) # new weight
                     #print weights[i] 
             
@@ -81,19 +84,24 @@ class Samme:
     
     def classify(self, data):
         
+        # Classify with all weak classifiers
+        all_classified = []
+        for c in range(len(self.classifiers)):
+            classifier = self.classifiers[c]
+            all_classified.append(classifier.classify(data[c]))
+        
+        # Order classification results according to the M selected classifiers
         wclassified = []
         for m in range(self.M):
-            wclassified.append(self.selected_classifiers[m].classify(data[m]))
-            
+            c = self.selected_classifiers[m]
+            wclassified.append(all_classified[c])
+          
+        # Calculate combined guess  
         classified = []
         for i in range(len(wclassified[0])):
             c = collections.defaultdict(lambda: 0)
             for m in range(self.M):
                 guess = wclassified[m][i][0][0]
-                #print guess
-                #print c
-                #print self.alphas
-                #print c[guess] 
                 c[guess] = c[guess] + self.alphas[m]
             best_guess = max(c.values())
             
